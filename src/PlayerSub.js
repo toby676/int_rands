4bec90f0ff2ba556fec80935ad3e6695c5dcb503
Toby Muir-Little <tjcmui@gmail.com>
2023-01-16 11:07:49 +0000

    Clean up listeners and unsub on unmount

--- src/PlayerSub.js
+++ src/PlayerSub.js
@@ -1,28 +1,34 @@
-import React, { useEffect, useState } from 'react'
+import React, {useEffect, useState} from 'react'
 import PubNub from 'pubnub'
-export default function PlayerSub(props){
-    const [response, setResponse] = useState("Attempt 0, You are team: \n Australia!")
-    var pubnub = new PubNub({
-        subscribeKey: process.env.REACT_APP_SUB_KEY,
-    })
-    useEffect(() => {
-        pubnub.addListener({
-            status: function(statusEvent) {
-                if (statusEvent.category === "PNConnectedCategory") {
-                }
-            },
-            message: function(msg) {
-                setResponse(msg.message.text)
-            }
-        })
-        pubnub.subscribe({
-            channels: [props.channel + props.id]
-        });
-    })
-    return(
-        <h1>
-            {props.name} <p/>
-            {response}
-        </h1>
-    )
+
+export default function PlayerSub(props) {
+  const [response, setResponse] = useState("Attempt 0, You are team: \n Australia!")
+  var pubnub = new PubNub({
+    subscribeKey: process.env.REACT_APP_SUB_KEY,
+  })
+  useEffect(() => {
+    const listener = {
+      status: function (statusEvent) {
+        if (statusEvent.category === "PNConnectedCategory") {
+        }
+      },
+      message: function (msg) {
+        setResponse(msg.message.text)
+      }
+    }
+    pubnub.addListener(listener)
+    pubnub.subscribe({
+      channels: [props.channel + props.id]
+    });
+    return () => {
+      pubnub.removeListener(listener)
+      pubnub.unsubscribeAll()
+    }
+  }, [])
+  return (
+    <h1>
+      {props.name} <p/>
+      {response}
+    </h1>
+  )
 }
--- src/WorldCupPlayerSub.js
+++ src/WorldCupPlayerSub.js
@@ -1,27 +1,33 @@
-import React, { useEffect, useState } from 'react'
+import React, {useEffect, useState} from 'react'
 import PubNub from 'pubnub'
-export default function PlayerSub(props){
-    const [response, setResponse] = useState("Click Select a match below!")
-    var pubnub = new PubNub({
-        subscribeKey: process.env.REACT_APP_SUB_KEY,
-    })
-    useEffect(() => {
-        pubnub.addListener({
-            status: function(statusEvent) {
-                if (statusEvent.category === "PNConnectedCategory") {
-                }
-            },
-            message: function(msg) {
-                setResponse(msg.message.text)
-            }
-        })
-        pubnub.subscribe({
-            channels: [props.channel + props.id]
-        });
-    })
-    return(
-        <h1>
-            {response}
-        </h1>
-    )
+
+export default function PlayerSub(props) {
+  const [response, setResponse] = useState("Click Select a match below!")
+  var pubnub = new PubNub({
+    subscribeKey: process.env.REACT_APP_SUB_KEY,
+  })
+  useEffect(() => {
+    const listener = {
+      status: function (statusEvent) {
+        if (statusEvent.category === "PNConnectedCategory") {
+        }
+      },
+      message: function (msg) {
+        setResponse(msg.message.text)
+      }
+    }
+    pubnub.addListener(listener)
+    pubnub.subscribe({
+      channels: [props.channel + props.id]
+    });
+    return () => {
+      pubnub.removeListener(listener)
+      pubnub.unsubscribeAll()
+    }
+  }, [])
+  return (
+    <h1>
+      {response}
+    </h1>
+  )
 }
